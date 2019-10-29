4bcd9f6b7ade6aacf26b537eccda5a713b443f7c
minemaster1329 <minemaster1329@onet.com.pl>
2019-10-29 23:43:17 +0100

    Initial Commit

--- TurtleApp/MainWindow.xaml.cs
+++ TurtleApp/MainWindow.xaml.cs
@@ -24,5 +24,202 @@ namespace TurtleApp
         {
             InitializeComponent();
         }
+
+        private void DrawButon_Click(object sender, RoutedEventArgs e)
+        {
+            int iterCount = 0;
+            double sideLenght = 0.0;
+
+            if (!Int32.TryParse(IterCountTxtb.Text, out iterCount) || iterCount < 1)
+            {
+                MessageBox.Show("Invalid Input format. Iterations count must be in integer format and greater than 1.");
+                return;
+            }
+
+            if (!Double.TryParse(SideLenghtTxtb.Text, out sideLenght) || sideLenght < 0)
+            {
+                MessageBox.Show("Invalid input format. Side lenght must be in double format and greater than 0.");
+            }
+
+            TurtleCanvas.ClearCanvas();
+
+            switch (ChooseFunctionComboBox.SelectedIndex)
+            {
+                case 1:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA1(iterCount, sideLenght);
+                    break;
+
+                case 2:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA2(iterCount, sideLenght);
+                    break;
+
+                case 3:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA3(iterCount, sideLenght);
+                    break;
+
+                case 4:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA4(iterCount, sideLenght);
+                    break;
+
+                case 5:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA5(iterCount, sideLenght);
+                    break;
+
+                case 6:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA6(iterCount, sideLenght);
+                    break;
+
+                case 7:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA7(iterCount, sideLenght);
+                    break;
+
+                case 8:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA8(iterCount, sideLenght);
+                    break;
+
+                case 9:
+                    TurtleCanvas.PosTurtle(TurtleCanvas.TurtlePos.MiddleCenter);
+                    TaskA9(iterCount, sideLenght);
+                    break;
+
+                default:
+                    MessageBox.Show("Please select function from list.");
+                    break;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            TurtleCanvas.ClearCanvas();
+        }
+
+        private void SierpinskiTriangle(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 3; a++)
+            {
+                TurtleCanvas.Forward(sideLen);
+                TurtleCanvas.Left(120);
+                SierpinskiTriangle(iter - 1, sideLen / 2);
+            }
+        }
+
+        private void TaskA1(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 4; a++)
+            {
+                TurtleCanvas.Forward(sideLen);
+                TaskA1(iter - 1, sideLen / 2);
+                TurtleCanvas.Right(90);
+            }
+        }
+
+        private void TaskA2(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 3; a++)
+            {
+                TurtleCanvas.Forward(sideLen);
+                TurtleCanvas.Right(120);
+                TaskA2(iter - 1, sideLen / 2);
+                TurtleCanvas.Right(120);
+            }
+        }
+
+        private void TaskA3(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 4; a++)
+            {
+                TurtleCanvas.Forward(sideLen);
+                TurtleCanvas.Right(90);
+                TaskA3(iter - 1, sideLen / 2);
+                TurtleCanvas.Left(180);
+            }
+        }
+
+        private void TaskA4(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 3; a++)
+            {
+                TurtleCanvas.Forward(sideLen);
+                TurtleCanvas.Right(60);
+                TaskA4(iter - 1, sideLen / 2);
+                TurtleCanvas.Left(180);
+            }
+        }
+
+        private void TaskA5(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 3; a++)
+            {
+                TurtleCanvas.Forward(sideLen);
+                TurtleCanvas.Left(180);
+                TaskA5(iter - 1, sideLen / 2);
+                TurtleCanvas.Right(60);
+            }
+        }
+
+        private void TaskA6(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 4; a++)
+            {
+                TurtleCanvas.Forward(sideLen / 4);
+                TurtleCanvas.Left(90);
+                TaskA6(iter - 1, sideLen / 2);
+                TurtleCanvas.Right(90);
+                TurtleCanvas.Forward(3 * sideLen / 4);
+                TurtleCanvas.Right(90);
+            }
+        }
+
+        private void TaskA7(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 3; a++)
+            {
+                TurtleCanvas.Forward(sideLen / 4);
+                TurtleCanvas.Left(60);
+                TaskA7(iter - 1, sideLen / 2);
+                TurtleCanvas.Right(60);
+                TurtleCanvas.Forward(3 * sideLen / 4);
+                TurtleCanvas.Right(120);
+            }
+        }
+
+        private void TaskA8(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            for (int a = 0; a < 6; a++)
+            {
+                TurtleCanvas.Forward(sideLen / 4);
+                TurtleCanvas.Left(120);
+                TaskA8(iter - 1, sideLen / 2);
+                TurtleCanvas.Right(120);
+                TurtleCanvas.Forward(3 * sideLen / 4);
+                TurtleCanvas.Right(60);
+            }
+        }
+
+        private void TaskA9(int iter, double sideLen)
+        {
+            if (iter == 0) return;
+            TurtleCanvas.Right(90);
+            TurtleCanvas.Forward(sideLen);
+            TurtleCanvas.Left(180);
+            TaskA9(iter - 1, sideLen + 10);
+        }
     }
 }
---
+++ TurtleApp/TurtleCanvas.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TurtleApp
+{
+    /// <summary>
+    /// Interaction logic for TurtleCanvas.xaml
+    /// </summary>
+    /// 
+
+    public partial class TurtleCanvas : UserControl
+    {
+        public enum TurtlePos { TopLeft, TopCenter, TopRight, MiddleLeft, MiddleCenter, MiddleRight, BottomLeft, BottomCenter, BottomRight };
+        Point turtlePos;
+        TurtlePos turtleDefaultPos;
+        double rotation;
+
+        public TurtleCanvas()
+        {
+            InitializeComponent();           
+            rotation = 0.0d;
+            turtleDefaultPos = TurtlePos.BottomCenter;
+        }
+
+        public void Right(double angle)
+        {
+            rotation += angle;
+            rotation %= 360;
+        }
+
+        public void Left(double angle)
+        {
+            rotation -= angle;
+            if (rotation < 0)
+            {
+                rotation += 360;
+            }
+        }
+
+        public void Forward(double length)
+        {
+            Line line = new Line();
+            line.Stroke = Brushes.Black;
+
+            line.X1 = turtlePos.X;
+            line.Y1 = turtlePos.Y;
+
+            line.X2 = turtlePos.X + Math.Cos((rotation * 2 * Math.PI) / 360) * length;
+            line.Y2 = turtlePos.Y + Math.Sin((rotation * 2 * Math.PI) / 360) * length;
+
+            line.StrokeThickness = 2;
+            Canvas.Children.Add(line);
+            turtlePos.X += Math.Cos((rotation * 2 * Math.PI) / 360) * length;
+            turtlePos.Y += Math.Sin((rotation * 2 * Math.PI) / 360) * length;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            PosTurtle(turtleDefaultPos);
+        }
+
+        public void ClearCanvas()
+        {
+            for (int i = Canvas.Children.Count - 1; i >= 0; i--)
+            {
+                UIElement child = Canvas.Children[i];
+                Canvas.Children.Remove(child);
+            }
+
+            PosTurtle(turtleDefaultPos);
+            rotation = 0;
+        }
+
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            PosTurtle(turtleDefaultPos);
+        }
+
+        public void PosTurtle(TurtlePos newPos)
+        {
+            switch (newPos)
+            {
+                //BOTTOM PART OF CANVAS
+                case TurtlePos.BottomLeft:
+                    turtlePos = new Point(0, Canvas.ActualHeight);
+                    break;
+
+                case TurtlePos.BottomCenter:
+                    turtlePos = new Point(Canvas.ActualWidth / 2, Canvas.ActualHeight);
+                    break;
+
+                case TurtlePos.BottomRight:
+                    turtlePos = new Point(Canvas.ActualWidth, Canvas.ActualHeight);
+                    break;
+
+                //MIDDLE PART OF CANVAS
+                case TurtlePos.MiddleLeft:
+                    turtlePos = new Point(0, Canvas.ActualHeight/2);
+                    break;
+
+                case TurtlePos.MiddleCenter:
+                    turtlePos = new Point(Canvas.ActualWidth / 2, Canvas.ActualHeight / 2);
+                    break;
+
+                case TurtlePos.MiddleRight:
+                    turtlePos = new Point(Canvas.ActualWidth / 2, Canvas.ActualHeight / 2);
+                    break;
+
+                //TOP PART OF CANVAS
+                case TurtlePos.TopLeft:
+                    turtlePos = new Point(0, 0);
+                    break;
+
+                case TurtlePos.TopCenter:
+                    turtlePos = new Point(Canvas.ActualWidth / 2, 0);
+                    break;
+
+                case TurtlePos.TopRight:
+                    turtlePos = new Point(Canvas.ActualWidth, 0);
+                    break;
+            }
+
+            turtleDefaultPos = newPos;
+        }
+    }
+}
